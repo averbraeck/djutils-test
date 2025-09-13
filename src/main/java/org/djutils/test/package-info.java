0d6b31123ed34f437098445c44c8111a9678c8c7
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-09-13 23:58:55 +0200

    Issue #1. Add package-info.java

---
+++ src/main/java/org/djutils/test/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Utilities such as UnitTest to help with exceptions. UnitTest.testFail(...) can reduce many lines of code with try...catch
+ * statements to single lines of code.
+ * <p>
+ * Copyright (c) 2025-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>.
+ * </p>
+ */
+package org.djutils.test;
